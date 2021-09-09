fe9a6f707eb578a1fbfd1f38e64737f1ca182f8f
Evgeniya Khramova <69902784+EvgeniyaKh@users.noreply.github.com>
2021-09-09 22:00:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let userName = getUserName();
 let githubAPIURL = 'https://api.github.com/users/';
-if (userName !== undefined && userName.trim() != '') {
+if (userName !== undefined && userName.trim() !== '') {
   let avatar = document.getElementById('avatar');
   let profileDescription = document.getElementById('description');
   let profileLink = document.getElementById('link');
@@ -14,7 +14,7 @@ function getUserName() {
   let searchURL = window.location.toString();
   let parseURL = searchURL.split('=');
   let name = parseURL[1];
-  if (name == undefined) {
+  if (name === undefined) {
     name = 'EvgeniyaKh';
   }
   return name;
@@ -22,14 +22,6 @@ function getUserName() {
 
 
 function getUser (avatar, profileDescription, profileLink, currentDateHTML) {
-  const load = document.getElementById('1');
-  const loading = new Promise((resolve, redject) => {
-    setTimeout(() => {
-      resolve(load.classList.add('not_active'));
-      redject(console.log('Что-то пошло не так'))
-    }, 4000);
-  });
-
   const currentDate = new Promise ((resolve, redject) => {
     setTimeout(() => {
       let date = new Date();
@@ -42,7 +34,7 @@ function getUser (avatar, profileDescription, profileLink, currentDateHTML) {
         case 1: fMonth="февраля"; break;
         case 2: fMonth="марта"; break;
         case 3: fMonth="апреля"; break;
-        case 4: fMonth="мае"; break;
+        case 4: fMonth="мая"; break;
         case 5: fMonth="июня"; break;
         case 6: fMonth="июля"; break;
         case 7: fMonth="августа"; break;
@@ -58,19 +50,24 @@ function getUser (avatar, profileDescription, profileLink, currentDateHTML) {
     }, 4000);
   });
 
-  Promise.all ([loading, currentDate])
-    .then (([loadPage, currentDate]) => fetch(githubAPIURL + userName)) 
+  Promise.all ([currentDate])
+    .then (([currentDate]) => fetch(githubAPIURL + userName)) 
     .then (res =>  res.json())
     .then (json => {
-      if(json.message != undefined) {
+      if (json.message !== undefined) {
         console.log(json.message);
         profileDescription.innerHTML = `Пользователя с ником ${userName} не существует`;
       } else {
-        profileDescription.innerHTML =  (json.bio != null) ? json.bio : "Нет данных";
+        profileDescription.innerHTML =  (json.bio !== null) ? json.bio : "Нет данных";
         profileLink.href = json.html_url;
-        profileLink.innerText = (json.name != null)? json.name : "Нет данных";
+        profileLink.innerText = (json.name !== null) ? json.name : "Нет данных";
         avatar.src = json.avatar_url;
       }
     })
+    .then (() => {
+      const load = document.getElementById('1');
+      const loading = load.classList.add('not_active');
+    })
+
   .catch(err => console.log('Информация о пользователе не доступна ' + err));
 }
